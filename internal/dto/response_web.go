972710d98bd394fbc3af45060612418aec334484
lawrient <sandincl.d@gmail.com>
2026-05-08 08:38:56 +0800

    fix: addjust response

--- internal/dto/response_web.go
+++ internal/dto/response_web.go
@@ -2,11 +2,5 @@ package dto
 
 type ResponseWeb[T any] struct {
 	Message string `json:"message"`
-	Data    T      `json:"data"`
-}
-
-type OldResponseWeb[T any] struct {
-	Message      string `json:"message"`
-	Success      bool   `json:"success"`
-	ResponseData T      `json:"response_data"`
+	Data    T      `json:"data,omitempty"`
 }
